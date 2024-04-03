e1151f9117ec21cdeb8bd57f2ba1b5b9f1a7de55
akshtop2025 <akshpateltop2025@gmail.com>
2024-04-03 17:37:34 +0530

    use api for update insert get

--- htdocs/formdata/functions.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-    function escape($data){
-		return htmlentities(strip_tags($data));
-	}
-
-	
-?>
--- htdocs/formdata/logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require_once 'DBConfigs.php';
-$res = $db->logout();
-
-if($res == true){
-	$db->redirect('login.php');
-}
-?>
